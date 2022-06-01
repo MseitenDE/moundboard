fc91146230ae8e7281e1d89a5aefb51d33e5dd62
MseitenDE <martin@mseiten.de>
2022-06-01 18:41:41 +0200

    Added Sound and AudioPlayer

---
+++ MoundBoard/AudioPlayer.cs
@@ -0,0 +1,31 @@
+﻿using NAudio.Wave;
+
+namespace MoundBoard;
+
+public class AudioPlayer
+{
+    public Sound Sound { get; }
+    public Mp3FileReader Reader { get; }
+    public WaveOut Output { get; }
+
+    public AudioPlayer(Sound sound)
+    {
+        Sound = sound;
+        Reader = new Mp3FileReader(sound.FilePath);
+        Output = new WaveOut();
+        
+        Output.PlaybackStopped += OutputOnPlaybackStopped;
+    }
+
+    public void Play()
+    {
+        Output.Init(Reader);
+        Output.Play();
+    }
+    
+    private void OutputOnPlaybackStopped(object? sender, StoppedEventArgs e)
+    {
+        Reader.Dispose();
+        Output.Dispose();
+    }
+}
