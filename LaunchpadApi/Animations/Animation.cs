164cdcbf804b52abe80859d3f35894373242686b
MseitenDE <martin@mseiten.de>
2022-06-10 16:23:44 +0200

    Added Animations

---
+++ LaunchpadApi/Animations/Animation.cs
@@ -0,0 +1,49 @@
+﻿using LaunchpadApi.Entities;
+
+namespace LaunchpadApi.Animations;
+
+public abstract class Animation
+{
+    /// <summary>
+    /// Delay in ms between frames
+    /// </summary>
+    public int Delay { get; }
+    public Layout? Layout { get; private set; }
+
+    /// <summary>
+    /// Creates a new animation
+    /// </summary>
+    /// <param name="delay">Delay in ms between frames</param>
+    protected Animation(int delay)
+    {
+        Delay = delay;
+    }
+    
+    /// <summary>
+    /// Starts the animation and applies the frames after the given time.
+    /// </summary>
+    public void Start(Layout layout)
+    {
+        Layout = layout;
+        OnStart(layout);
+
+        while (HasNext())
+        {
+            ApplyNextFrame();
+            Thread.Sleep(Delay);
+        }
+    }
+    
+    /// <summary>
+    /// Starts the animation, but new frames are only displayed if <see cref="ApplyNextFrame"/> is called.
+    /// </summary>
+    public void StartManual(Layout layout)
+    {
+        Layout = layout;
+        OnStart(layout);
+    }
+
+    protected abstract void OnStart(Layout layout);
+    public abstract void ApplyNextFrame();
+    public abstract bool HasNext();
+}
---
+++ LaunchpadApi/Animations/ShiftAnimation.cs
@@ -0,0 +1,57 @@
+﻿using LaunchpadApi.Entities;
+using MoundBoard.Core;
+
+namespace LaunchpadApi.Animations;
+
+public class ShiftAnimation : Animation
+{
+    public int AnimationLength { get; private set; }
+    public Colors[,] Colors { get; }
+    public int Frame { get; private set; }
+    public bool Repeat { get; set; }
+    private int _offset;
+
+    public ShiftAnimation(int delay, Colors[,] colors) : base(delay)
+    {
+        Colors = colors;
+    }
+
+    protected override void OnStart(Layout layout)
+    {
+        _offset = layout.Launchpad.ColumnCount;
+        AnimationLength = Colors.GetLength(1) + _offset;
+    }
+
+    public override void ApplyNextFrame()
+    {
+        for (var x = 0; x < Layout!.Launchpad.ColumnCount; x++)
+        {
+            for (var y = 0; y < Layout.Launchpad.RowCount; y++)
+            {
+                Colors color;
+                if (x < Layout.Launchpad.RowCount - Frame)
+                {
+                    color = MoundBoard.Core.Colors.Black;
+                }
+                else if (x < AnimationLength - Frame)
+                {
+                    color = Colors[Layout.Launchpad.RowCount - 1 - y, x + Frame - _offset];
+                }
+                else
+                {
+                    color = MoundBoard.Core.Colors.Black;
+                }
+
+                Layout![x, y].Color = color;
+            }
+        }
+        Frame++;
+
+        if (Repeat && Frame >= AnimationLength) Frame = 0;
+    }
+
+    public override bool HasNext()
+    {
+        return Frame <= AnimationLength;
+    }
+}
