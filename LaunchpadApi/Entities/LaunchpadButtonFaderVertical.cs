6b7d85065dda836a57ef7faf9096090a56c680be
Mark <mark.stroebele@t-online.de>
2022-07-09 10:30:22 +0200

    Fader with Problem, LayoutFader soll von Layout erben aber Error

---
+++ LaunchpadApi/Entities/LaunchpadButtonFaderVertical.cs
@@ -0,0 +1,54 @@
+﻿using Windows.Devices.Midi;
+using LaunchpadApi.Animations;
+using LaunchpadApi.Utils;
+using MoundBoard.Core;
+
+namespace LaunchpadApi.Entities;
+
+public class LaunchpadButtonFaderVertical
+{
+    private Colors? _color = Colors.Green;
+    public Layout Layout { get; }
+    public byte X { get; }
+    public byte Y { get; }
+    public LaunchpadButton[] Buttons { get; }
+    public ButtonEffect Effect { get; set; }
+    private byte NoteIndex => (byte) (X + Y * Layout.Launchpad.RowCount);
+
+    public Colors? Color
+    {
+        get => _color;
+        set
+        {
+            if (_color == value) return;
+            _color = value ?? Colors.Black;
+            SendUpdate();
+        }
+    }
+
+    public LaunchpadButtonFaderVertical(Layout layout, byte x)
+    {
+        Layout = layout;
+        X = x;
+        Y = 1;
+        Buttons![0] = new LaunchpadButton(layout, X, Y);
+        Buttons[1] = new LaunchpadButton(layout, X, (byte)(Y + 10));
+        Buttons[2] = new LaunchpadButton(layout, X, (byte)(Y + 20));
+        Buttons[3] = new LaunchpadButton(layout, X, (byte)(Y + 30));
+        Buttons[4] = new LaunchpadButton(layout, X, (byte)(Y + 40));
+        Buttons[5] = new LaunchpadButton(layout, X, (byte)(Y + 50));
+        Buttons[6] = new LaunchpadButton(layout, X, (byte)(Y + 60));
+        Buttons[7] = new LaunchpadButton(layout, X, (byte)(Y + 70));
+    }
+
+    /// <summary>
+    /// Updates the button on the launchpad, ignoring if 
+    /// </summary>
+    public void SendUpdate()
+    {
+        if (!Layout.IsActive) return;
+
+        var noteMessage = new MidiNoteOnMessage((byte) Effect, NoteIndex, Color.Convert());
+        Layout.Launchpad.OutPort.SendMessage(noteMessage);
+    }
+}
---
+++ LaunchpadApi/Entities/LayoutFader.cs
@@ -0,0 +1,54 @@
+﻿using MoundBoard.Core;
+
+namespace LaunchpadApi.Entities;
+
+public class LayoutFader : Layout
+{
+    public Launchpad Launchpad { get; }
+    public bool IsActive { get; protected internal set; }
+    public string Name { get; }
+    public LaunchpadButtonFaderVertical[] Fader { get; }
+    
+
+    public LayoutFader(string name, Launchpad launchpad)
+    {
+        Name = name;
+        Launchpad = launchpad;
+        
+        for (byte x = 0; x < launchpad.ColumnCount; x++)
+        {
+            
+            Fader[x] = new LaunchpadButtonFaderVertical(this, x);
+            // for (byte y = 0; y < launchpad.RowCount; y++)
+            // {
+            //     Buttons[x, y] = new LaunchpadButton(this, x, y);
+            // }
+        }
+    }
+
+    public void SetSolidColor(Colors color)
+    {
+        ApplyForAll(button => button.Color = color);
+    }
+
+    public void ApplyForAll(Action<LaunchpadButton> button)
+    {
+        for (var x = 0; x < Launchpad.ColumnCount; x++)
+        {
+            for (var y = 0; y < Launchpad.RowCount; y++)
+            {
+                button.Invoke(Buttons[x, y]);
+            }
+        }
+    }
+    
+    public LaunchpadButton this[int x, int y] => Buttons[x, y];
+
+    /// <summary>
+    /// Force updates all buttons
+    /// </summary>
+    public void Apply()
+    {
+        ApplyForAll(button => button.SendUpdate());
+    }
+}
